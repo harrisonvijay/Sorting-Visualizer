5971f35183ef900aae399bb9e63f61e8bb94e503
harrisonvijay <vijay.harrison@gmail.com>
2021-06-25 12:34:35 +0530

    Restricted smaller screens

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <SortingVisualizer />
-    </div>
+    <>
+      <div className="App">
+        <SortingVisualizer />
+      </div>
+      <div className="overlay">
+        <span>This app is only supported in displays with width greater than 900px</span>
+      </div>
+    </>
   );
 }
 
